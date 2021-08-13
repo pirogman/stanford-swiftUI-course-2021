35986676c7c8e5a29b90536c9c39f17cf382e9dc
Oleksandr Pyroh <opyroh@zeushq.com>
2021-08-13 10:41:41 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Oleksandr Pyroh on 13.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
